fa64f70ee243d0a3f956a639b98664e8b50a01c8
unknown <varikvi@yahoo.com>
2024-10-12 23:55:17 +0200

    SCRUM 6: cleaning the code, refactoring

--- sketchcalc/main.js
+++ sketchcalc/main.js
@@ -30,10 +30,13 @@ let dataset = [
 
 /* DATA COLLECTORS and CONSTANTS */
 
+let outputs = {};
+
 let selectedCells = {
-    col: null,
-    rows: [],
-    htmlElems: []
+    checkedColId: null,
+    checkedRowIds: [],
+    rowCount: -1,
+    editableCells: []
 }
 
 let updatedContribs = {} 
@@ -44,20 +47,33 @@ const cTot = Number(document.getElementById("tot").innerHTML);
 
 let numberOfSplits = 0;
 
+/* CONSTANTS and GLOBAL-USE ELEMENTS */
+
+const table = document.querySelector("table");
+
+let getOutputs = function(selector){return document.querySelectorAll(selector)};
 
 /* FUNCTIONALITIES */
 
-let init = function(){ //create table
-    let table = document.querySelector("table");
-    let rowCount = table.rows.length;
+let populateFunc = function(){ //create table
 
+    let rowCount = table.rows.length;
     let eqCon = cTot/4;
     let eqPer = eqCon*100/cTot;
 
     for(let rcdIdx = 0; rcdIdx < dataset.length; rcdIdx++){
+        
+        //insert a new row
+
         let rcdRow = table.insertRow(rowCount);
         rcdRow.id =  dataset[rcdIdx].recordId;
 
+        //start populating updateContribs with the record ids as keys
+        
+        updatedContribs[ dataset[rcdIdx].recordId] = {};
+
+        //prepare the correponding cells, specific to each column
+        
         let rcdCBoxCell = rcdRow.insertCell(0);
         rcdCBoxCell.id = dataset[rcdIdx].recordId + '-checkboxCell';
         rcdCBoxCell.headers = 'checkboxesTbCol';
@@ -70,31 +86,41 @@ let init = function(){ //create table
 
         let rcdRelSplCell = rcdRow.insertCell(2);
         rcdRelSplCell.id = dataset[rcdIdx].recordId + '-relsplCell';
-        rcdRelSplCell.headers = 'relativeSplitTbCol';
-        rcdRelSplCell.innerHTML = `<input class='edit' type='number' name='spl' id='${dataset[rcdIdx].recordId}-editRelSpl' min='0' max='100'><span class='output' id='${dataset[rcdIdx].recordId}-outRelSpl'>${eqPer}</span>`;
+        rcdRelSplCell.headers = 'editRelSpl';
+        rcdRelSplCell.innerHTML = `<input class='editable editRelSpl' type='number' name='spl' id='${dataset[rcdIdx].recordId}-editRelSpl' min='0' max='100'><span class='output outRelSpl' id='${dataset[rcdIdx].recordId}-outRelSpl'>${eqPer}</span>`;
 
         let rcdAbsSplCell = rcdRow.insertCell(3);
         rcdAbsSplCell.id = dataset[rcdIdx].recordId + '-abssplCell';
-        rcdAbsSplCell.headers = 'absoluteSplitTbCol';
-        rcdAbsSplCell.innerHTML = `<input class='edit' type='number' name='spl' id='${dataset[rcdIdx].recordId}-editAbsSpl' min='0' max='100'><span class='output' id='${dataset[rcdIdx].recordId}-outAbsSpl'>${eqCon}</span>`;
+        rcdAbsSplCell.headers = 'editAbsSpl';
+        rcdAbsSplCell.innerHTML = `<input class='editable editAbsSpl' type='number' name='spl' id='${dataset[rcdIdx].recordId}-editAbsSpl' min='0' max='100'><span class='output outAbsSpl' id='${dataset[rcdIdx].recordId}-outAbsSpl'>${eqCon}</span>`;
 
         let rcdPaidCell = rcdRow.insertCell(4);
-        rcdPaidCell.id = dataset[rcdIdx].recordId + '-checkboxCell';
-        rcdPaidCell.headers = 'paidTbCol';
-        rcdPaidCell.innerHTML = `<input class='edit' type='number' name='spl' id='${dataset[rcdIdx].recordId}-editPaid' min='0' max='100'><span class='output' id='${dataset[rcdIdx].recordId}-outPaid'>0.00</span>`;
+        rcdPaidCell.id = dataset[rcdIdx].recordId + '-rcdPaidCell';
+        rcdPaidCell.headers = 'editPaid';
+        rcdPaidCell.innerHTML = `<input class='editable editPaid' type='number' name='spl' id='${dataset[rcdIdx].recordId}-editPaid' min='0' max='100'><span class='output outPaid' id='${dataset[rcdIdx].recordId}-outPaid'>0.00</span>`;
 
         let rcdAbsRemCell = rcdRow.insertCell(5);
-        rcdAbsRemCell.id = dataset[rcdIdx].recordId + '-checkboxCell';
+        rcdAbsRemCell.id = dataset[rcdIdx].recordId + '-rcdAbsRemCell';
         rcdAbsRemCell.headers = 'absoluteRemainingTbCol';
-        rcdAbsRemCell.innerHTML = `<span class='output' id='${dataset[rcdIdx].recordId}-outAbsRem'>${eqCon}</span>`;
+        rcdAbsRemCell.innerHTML = `<span class='output outAbsRem' id='${dataset[rcdIdx].recordId}-outAbsRem'>${eqCon}</span>`;
 
         let rcdRelRemCell = rcdRow.insertCell(6);
-        rcdRelRemCell.id = dataset[rcdIdx].recordId + '-checkboxCell';
+        rcdRelRemCell.id = dataset[rcdIdx].recordId + '-rcdRelRemCell';
         rcdRelRemCell.headers = 'relativeRemainingTbCol';
-        rcdRelRemCell.innerHTML = `<span class='output' id='${dataset[rcdIdx].recordId}-outRelRem'>${eqPer}</span>`;
+        rcdRelRemCell.innerHTML = `<span class='output outRelRem' id='${dataset[rcdIdx].recordId}-outRelRem'>${eqPer}</span>`;
+
+        //go to the following row
 
         rowCount = table.rows.length;
     }
+
+    ////get all initial outputs
+    
+    //outputs.outRelSplFunc = document.querySelectorAll('.outRelSpl');
+    //outputs.outAbsSplFunc = document.querySelectorAll('.outAbsSpl');
+    //outputs.outPaidFunc = document.querySelectorAll('.outPaid');
+    //outputs.outRelRemFunc = document.querySelectorAll('.outRelRem');
+    //outputs.outAbsRemFunc = document.querySelectorAll('.outAbsRem');
 }
 
 function __getCheckedColRows(){
@@ -102,37 +128,87 @@ function __getCheckedColRows(){
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
     const checkedCol = !radios? [] : Array.from(radios).filter((r,i)=>{return r.checked;});
     const checkedRows = !checkboxes? [] : Array.from(checkboxes).filter((cbox,i)=>{return cbox.checked;});
-    return checkedCol, checkedRows;
+
+    //collect colid, rowids, length of the table and all editable cells
+
+    selectedCells.checkedColId = checkedCol[0].id;
+    selectedCells.checkedRowIds = checkedRows.map((v,i)=> {return (v.id).split('-')[0]});
+    selectedCells.rowCount = table.rows.length;
+    selectedCells.editableCells = document.querySelectorAll("." + selectedCells.checkedColId);
 }
 
-let editBtn = function(){
-    let inputAll = document.getElementsByTagName("input");
-    let {checkedCol, checkedRows} = __getCheckedColRows();
-    //console.log(checkedboxs, checkedradio);
+let editBtnFunc = function(){
+    
+    //populate selectedCells
+    
+    __getCheckedColRows();
 
-    let rowscols = checkedboxs.map((v,i)=>{ if(v){
-                return v+checkedradio[0];
-            }});
-    //console.log(rowscols);
+    //go through all editable cells; if marked as selected, display the Edit field
 
-    for(let i = 0; i < inputAll.length; i++){
-
-        //update all inputs to close
-        if(!(inputAll[i].value).includes('row') && !(inputAll[i].value).includes('col') && !(inputAll[i].value).includes('form')){
-            inputAll[i].style.display = 'none';
-        }                
-        
-        //open only those required
-        if(checkedboxs.length > 0 && checkedradio.length > 0){
-            for(let j = 0; j < rowscols.length; j++){
-                if((inputAll[i].id).includes(rowscols[j] + 'in')){
-                    inputAll[i].style.display = 'inline-block';
-                }
-            }
+    for(let editIdx = 0; editIdx < selectedCells.editableCells.length; editIdx++){
+        if(selectedCells.checkedRowIds.includes((selectedCells.editableCells[editIdx].id).split('-')[0])){
+            selectedCells.editableCells[editIdx].style.display = 'inline-block';
+        }else{
+            selectedCells.editableCells[editIdx].style.display = 'none';
         }
     }
 }
 
+let updateBtnFunc = function(){
+    
+    //is the AbsSpl col edited?
+    
+    if(selectedCells.checkedColId == 'editAbsSpl'){
+        
+        // get data from edited cells and keep it in updatedContribs
+
+        for(let editedIdx = 0; editedIdx <  selectedCells.editableCells.length; editedIdx++){
+            let key = (selectedCells.editableCells[editedIdx].id).split('-')[0];
+            newContrs[key].newAbsContr = [Number(selectedCells.editableCells[editedIdx].value)];
+
+            //just check if there is a value to see if edited
+
+            if(selectedCells.editableCells[editedIdx].value){
+                newContrs[key].edited = true;
+                newContrs[key].newRelContr = 100*newContrs[key].newAbsContr/cTot;
+            }else{
+                newContrs[key].edited = false;
+                newContrs[key].newRelContr = null;
+                numberOfSplits += 1;
+            }
+        }
+    }
+
+    //get current contr value and make a first collection of differences
+
+    let outputs = getOutputs('.outAbsSpl');
+
+    /* TODO
+        - push also outputs to newContrs[key].newAbsContr (line 218)
+        - if edited, push the difference with current output, otherwise 0
+
+        - sum new split (line 232)
+
+        - go through output again, to update the new values, where the new split is added to the non-edited (from line 236)
+        - recalcuate the percentage
+
+        - update percentages and differences with paid values
+
+    THEN
+        - do the opposite algorithm for updates on the percentage column
+
+    THEN
+        - do a simpler algorithm for the paid column
+    */
+    for(let outputIdx = 0; outputIdx <  outputs.length; outputIdx++){
+
+    }
+
+
+}
+
 window.onload = function(){
-    init();
+    populateFunc();
+    const editBtn = document.querySelector('#editBtn');
+    editBtn.addEventListener('click', editBtnFunc);
 }
